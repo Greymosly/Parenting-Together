8ebdfec94a092b46de16fa9ed66afaae18b9e0ac
John Greenlaw <37337264+Greymosly@users.noreply.github.com>
2021-05-21 18:13:19 -0600

    Update calendarItem.js

--- src/components/calendarItem.js
+++ src/components/calendarItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import Calendar from "@ericz1803/react-google-calendar";
 import { css } from "@emotion/react";
 
-const API_KEY = "AIzaSyCfKe5_FZWDA7IYAIok5_jQcw8tWjWIB_Q";
+const API_KEY = "";
 let calendars = [{ calendarId: "bottegajohngreenlaw@gmail.com" }];
 
 let styles = {
